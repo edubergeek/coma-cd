ecf866f4bb4699b6bdab2fcf17963fb36c917ffe
Curt Dodds <cdodds@planit.IfA.Hawaii.Edu>
2021-01-18 12:35:03 -1000

    Incorporated Jan's pipeline Added mapping to PDS4 bundle, collection and product Added date wrangling

--- cd.sh
+++ cd.sh
@@ -14,73 +14,136 @@ cp /dev/null /tmp/cd.err
 
 dirs="INCOMING VALIDATED"
 
+fitsinfo=$HOME/Projects/coma-cd/fitsinfo.sh
+
 function tree() {
 	find $1 -type 'd' -depth -print
+}
+
+function map_dir() {
+	case "$1" in
+	*[dD][aA][rR][kK]*)
+		product=dark
+		bundle=dark;;
+	*[bB][iI][aA][sS]*)
+		product=bias
+		bundle=bias;;
+	*[fF][lL][aA][tT]*)
+		product=flat
+		bundle=flat;;
+	*[tT][wW][iI]*)
+		product=twilight
+		bundle=flat;;
+	*)
+		product=data;;
+	esac
+
+	echo $product $bundle
+	return 0
+}
+	
+function map_telinst() {
+	case "$1-$2" in
+	"-") return 1;;
+
+	-*) collection="$2";;
+	*-) collection="$1";;
+	*) collection="$1-$2";;
+	esac
+
+	echo $collection
+	return 0
+}
+
+function map_object() {
+	case "$1" in
+	"") return 1;;
+
+	*[dD][aA][rR][kK]*)
+		product=dark
+		bundle=dark;;
+	*[bB][iI][aA][sS]*)
+		product=bias
+		bundle=bias;;
+	*[fF][lL][aA][tT]*)
+		product=flat
+		bundle=flat;;
+	*[tT][wW][iI][lL]*)
+		product=twilight
+		bundle=flat;;
+	*) bundle="$1";;
+	esac
+
+	echo $bundle $product
+	return 0
+}
+
+function toJD() {
+	echo $1
+	return 0
 }
 
 function scan() {
 	dir=$1
 	ls $dir | grep '\.' | while read fits
 	do
-		eval `gethead -e $dir/$fits @$RUNDIR/keys`
-		echo "$dir,$fits,$TELESCOP,$INSTRUME,$OBJECT,$JD,$DATE"
+		eval `bash $fitsinfo $dir/$fits`
+		# map subdirectory and file name to PRODUCT and/or BUNDLE
+		map=`map_dir "$dir"`
+		read PRODUCT BUNDLE <<< "$map"
 
-		nolink=0
-		case "$dir" in
-		"*[bB][iI][aA][sS]*") PRODUCT="cal";;
-		"*[fF][lL][aA][tT]*") PRODUCT="flat";;
-		"*[tT][wW][iI][lL]*") PRODUCT="flat";;
-		"*") PRODUCT="raw";;
-		esac
+		# map telescope and instrument into a PDS4 "collection"
+		COLLECTION=`map_telinst "$TELESCOP $INSTRUME"`
+		nolink=$?
 
-		case "$OBJECT" in
-		"") nolink=1;;
-		"*[bB][iI][aA][sS]*")
-			PRODUCT="cal"
-			BUNDLE="$OBJECT";;
-		"*[fF][lL][aA][tT]*")
-			PRODUCT="flat"
-			BUNDLE="$OBJECT";;
-		"*[tT][wW][iI][lL]*")
-			PRODUCT="flat"
-			BUNDLE="$OBJECT";;
-		"*") BUNDLE="$OBJECT";;
-		esac
+		# map $OBJECT to BUNDLE/PRODUCT
+		map=`map_object "$OBJECT"`
+		if [ $? -eq 0 ]
+		then
+			read BUNDLE PRODUCT <<< "$map"
+		else
+			nolink=$?
+		fi
 
-		case "$TELESCOP-$INSTRUME" in
-		"") nolink=1;;
-		"-*") COLLECTION="$INSTRUME";;
-		"*-") COLLECTION="$TELESCOP";;
-		"*") COLLECTION="$TELESCOP-$INSTRUME";;
-		esac
-
+		# convert mm/dd/yyyy format to JD (MJD?)
 		case "$JD-$DATE" in
-		"") nolink=1;;
-		"-*") ;;
-		"*-") DATE="$JD";;
-		"*") DATE="$JD";;
+		"") nolink=1 ;;
+		-*) JD=`toJD "$DATE"` ;;
 		esac
 
+		# If we can create a link do so otherwise print out what we have
 		case $nolink in
 		0)
-			mkdir -p $LINKDIR/$BUNDLE/$COLLECTION/$PRODUCT/$DATE
-			ln -s $dir/$fits $LINKDIR/$BUNDLE/$COLLECTION/$PRODUCT/$DATE/$fits
+			# For now just print it out
+			echo $BUNDLE $COLLECTION $PRODUCT $JD $dir/$fits
+
+			# make sure the bundle/collection/product path exists
+			#mkdir -p $LINKDIR/$BUNDLE/$COLLECTION/$PRODUCT/$JD
+
+			# link the FITS file there
+			#ln -s $dir/$fits $LINKDIR/$BUNDLE/$COLLECTION/$PRODUCT/$JD/$fits
+			;;
+		*)
+			# Link is not possible so dump what we have
+			echo obj=$OBJECT tel=$TELESCOP ins=$INSTRUM bun=$BUNDLE col=$COLLECTION pro=$PRODUCT jd=$JD fits=$dir/$fits >&2
 			;;
 		esac
 		break
 	done
 }
 
-echo "Dir,File,Telescope,Instrument,Object,JD,DATE"
+# process root directories INCOMING and VALIDATED
 for scan in $dirs
 do
 	cd ${SCANDIR}/$scan
+	# recursively descend through the current root directory
 	tree $SCANDIR/$scan | while read cdrom
 	do
 		cd $cdrom
 		ls $cdrom | grep '\.' >/dev/null 2>&1
 		case $? in
 		0)
+			# found a directory with files having extensions (presumably FITS) so scan it
 			scan $cdrom
 			;;
 		*)
